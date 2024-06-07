3c428b16762a0a68b87f0419072c62fae80afb61
brdarwin <160612209+brdarwin@users.noreply.github.com>
2024-06-06 21:01:20 -0300

    Add files via upload

--- ClearInbox.py
+++ ClearInbox.py
@@ -2,94 +2,84 @@ import pyautogui as pyg
 import time
 import os
 
-def clickingArea(imgPath):
-    try:
-        img  = pyg.locateOnScreen(imgPath)
-        xAxis = int (img[0] + (img[2] / 2))
-        yAxis = int (img[1] + (img[3] / 2))
-        pyg.click(xAxis, yAxis)
-    except pyg.ImageNotFoundException:
-        pass
-        
-        
-        
-        
-def searchingPage (theLink):
-    clickingArea("OpenPage.png")
+
+def checkImage(imgPath, op):
+    for i in range (0, 9):
+        try:
+            img  = pyg.locateOnScreen(imgPath)
+            if op == 0:
+                pyg.hotkey("Enter")
+            elif op == 1:
+                xAxis = int (img[0] + (img[2] / 2))
+                yAxis = int (img[1] + (img[3] / 2))
+                pyg.click(xAxis, yAxis)
+            else:
+                pass
+            break
+        except pyg.ImageNotFoundException:
+            time.sleep(5)
     time.sleep(3)
-    clickingArea("ExitPage.png")
+
+def searchingPage(theLink):
+    checkImage("OpenPage.png", 1)
+    time.sleep(3)
+    checkImage("ExitPage.png", 1)
     pyg.typewrite(theLink)
     time.sleep(1)
     pyg.hotkey("Enter")
-    
 
 def main():
     time.sleep(5)
     pyg.FAILSAFE = False
-    clickingArea("TopLeft.png")
+    checkImage("TopLeft.png", 1)
     time.sleep(1)
-    clickingArea("FireFox.png")
-    time.sleep(10)
+    checkImage("FireFox.png", 1)
     
     
     #Proton:
     searchingPage("https://account.proton.me/login")
-    time.sleep(15)
-    pyg.hotkey("Enter")
-    time.sleep(20)
-    clickingArea("ProtonMail.png")
-    time.sleep(15)
-    clickingArea("SelectAll.png")
-    time.sleep(1)
+    checkImage("ProtonEnter.png", 0)
+    checkImage("ProtonMail.png", 1)
+    checkImage("SelectAll.png", 1)
     pyg.hotkey("t")
+    
     
     #Gmail:
     searchingPage("https://www.google.com/")
-    time.sleep(20)
-    clickingArea("GLogin.png")
-    time.sleep(5)
+    checkImage("GLogin.png", 1)
     pyg.typewrite("bdarwin@acad.ifma.edu.br")
-    time.sleep(3)
-    pyg.hotkey("Enter")
-    time.sleep(3)
-    pyg.hotkey("Enter")
-    time.sleep(5)
-    clickingArea("EnterGmail.png")
-    time.sleep(45)
-    clickingArea("SelectAllGml.png")
-    time.sleep(10)
-    clickingArea("RubIcon.png")
-    time.sleep(10)
+    checkImage("EnterGoo.png", 0)
+    checkImage("EnterGoo.png", 0)
+    checkImage("EnterGmail.png", 1)
+    checkImage("SelectAllGml.png", 1)
+    checkImage("RubIcon.png", 1)
 
+    
     #Gpt:
     searchingPage("https://chatgpt.com/?oai-dm=1")
-    time.sleep(10)
-    clickingArea("LogGpt.png")
-    clickingArea("LogGpt.png")
-    time.sleep(5)
+    checkImage("LogGpt.png", 1)
+    checkImage("EnterGpt.png", 2)
     pyg.typewrite("brndarwin@proton.me")
     time.sleep(1)
     pyg.hotkey("Enter")
-    time.sleep(5)
-    pyg.hotkey("Enter")
-    time.sleep(10)
-    clickingArea("Profile.png")
-    time.sleep(2)
-    clickingArea("Settings.png")
-    time.sleep(2)
-    clickingArea("DeleteAll.png")
-    time.sleep(2)
-    clickingArea("ConfirmDelection.png")
-    '''
-    Stopped Here
+    checkImage("EnterPass.png", 0)
+    checkImage("Profile.png", 1)
+    checkImage("Settings.png", 1)
+    checkImage("DeleteAll.png", 1)
+    checkImage("ConfirmDelection.png", 1)
+    
+    
     #Outlook:
     searchingPage("https://outlook.live.com/mail/0/")
-    time.sleep(10)
-    clickingArea("SignIn.png")
-    time.sleep(5)
-    for i in range (0,3):
-        pyg.hotkey("Enter")
-        time.sleep(3)
-    '''
+    checkImage("SignIn.png", 1)
+    checkImage("Next.png", 0)
+    checkImage("EnterOutlook.png", 0)
+    checkImage("Yes.png", 0)  
+    checkImage("Select.png", 1) 
+    checkImage("SelectAllOut.png", 1)
+    pyg.hotkey("Delete")
+    checkImage("Yep.png", 1)
+    
+    
 
 main()
